783d1542d428e0a60ba542c7f8b485b015203edd
lian <1057313226@qq.com>
2021-05-23 09:27:47 +0800

    feat:new

---
+++ test/Classes/LYTestObject.h
@@ -0,0 +1,16 @@
+//
+//  LYTestObject.h
+//  test
+//
+//  Created by lian on 2021/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LYTestObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/Classes/LYTestObject.m
@@ -0,0 +1,12 @@
+//
+//  LYTestObject.m
+//  test
+//
+//  Created by lian on 2021/5/23.
+//
+
+#import "LYTestObject.h"
+
+@implementation LYTestObject
+
+@end
